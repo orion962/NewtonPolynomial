c2a4e02fc95ec3f0b18387e71652c6bf5ee479ef
orion962 <danilgalich111@gmail.com>
2023-10-23 17:02:18 +0300

    all files

---
+++ cmake-build-debug/CMakeFiles/NewtonPolynomial.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewtonPolynomial.dir/src/TableFun.cpp.obj"
+  "CMakeFiles/NewtonPolynomial.dir/src/main.cpp.obj"
+  "NewtonPolynomial.exe"
+  "NewtonPolynomial.exe.manifest"
+  "NewtonPolynomial.pdb"
+  "libNewtonPolynomial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewtonPolynomial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
